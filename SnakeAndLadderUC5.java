573edc0ef96a21f317c42008dcd6383c7eccd425
srinidhi <srikharpasunuri51@gmail.com>
2023-11-23 13:08:51 +0530

    added uc5

---
+++ SnakeAndLadderUC5.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class SnakeAndLadderUC5 {
+
+    public static final int Winning_Position = 100;
+
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String Player_Name = sc.next();
+
+        while (Winning_Position > startPosition) {
+
+            int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            int needToWin = Winning_Position - startPosition;
+            int checkOption = (int) (Math.floor(Math.random() * 10) % 3);
+
+            if(needToWin >= dieRoll) {
+
+                switch (checkOption) {
+                    case 0:
+                        System.out.println("No Play");
+                        startPosition += 0;
+                        break;
+
+                    case 1:
+                        System.out.println("Ladder:" + " + " + dieRoll);
+                        startPosition += dieRoll;
+                        break;
+
+                    default:
+                        System.out.println("Snake : " + " - " + dieRoll);
+                        startPosition -= dieRoll;
+
+                        if (startPosition < 0) {
+                            System.out.println("Start Again");
+                            startPosition = 0;
+                        }
+
+                }
+            }
+        }
+
+        System.out.println("Player " + Player_Name + "Reached the Winning Position :" + startPosition);
+
+
+    }
+}
