e1f9a3c03ac44358d670740464ae691fb87639b4
srinidhi <srikharpasunuri51@gmail.com>
2023-11-23 12:48:48 +0530

    added uc1

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+public class SnakeAndLadderUC1 {
+
+    public static void main(String [] args){
+
+        int startPosition = 0;
+        System.out.println("Starting Position is :" + startPosition);
+    }
+
+}
